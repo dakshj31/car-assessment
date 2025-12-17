3ae87313a80c9f9770499d1746637ec36b9db62f
Daksh Joshi <dakshjoshi1355@gmail.com>
2025-12-17 13:43:47 +0530

    changes

--- admin/header.php
+++ admin/header.php
@@ -1,23 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-<title>Admin Panel</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-
-<nav class="navbar navbar-dark bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href="dashboard.php">CarsDekho Admin</a>
-    <a href="../index.php" class="btn btn-outline-light btn-sm">View Website</a>
-    
-  </div>
-</nav>
-
-<div class="container my-4">
-
-<h2 class="mb-4 text-center">Admin Dashboard</h2> -->
-
 <?php
 session_start();
 if(!isset($_SESSION['admin'])){
